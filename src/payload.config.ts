f789d364a6bf67827c4bd7eaee20e7a9559eaf7c
soham mondal <sohammondal50285@gmail.com>
2026-03-14 17:57:39 +0530

    add code

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -28,7 +28,7 @@ export default buildConfig({
   },
 
   
-  
+
   cors: [
   "http://localhost:3000",
   "https://gresham-global.vercel.app"
@@ -69,67 +69,3 @@ export default buildConfig({
 
 
 
-
-
-// import { mongooseAdapter } from "@payloadcms/db-mongodb";
-// import { lexicalEditor } from "@payloadcms/richtext-lexical";
-// import path from "path";
-// import { buildConfig } from "payload";
-// import { fileURLToPath } from "url";
-// import sharp from "sharp";
-
-// import { Users } from "./collections/Users";
-// import { Media } from "./collections/Media";
-// import MediaNews from "./collections/MediaNews";
-// import NewsBlogs from "./collections/NewsBlogs";
-// import Publications from "./collections/Publications";
-
-// const filename = fileURLToPath(import.meta.url);
-// const dirname = path.dirname(filename);
-
-// // 🔍 log database URL
-// console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
-// export default buildConfig({
-
-//   admin: {
-//     user: Users.slug,
-//     importMap: {
-//       baseDir: path.resolve(dirname),
-//     },
-//   },
-
-//   cors: ["http://localhost:3000"],
-
-//   serverURL: "http://localhost:3001",
-
-//   collections: [
-//     Users,
-//     Media,
-//     MediaNews,
-//     NewsBlogs,
-//     Publications
-//   ],
-
-//   editor: lexicalEditor(),
-
-//   secret: process.env.PAYLOAD_SECRET || "payload-secret",
-
-//   typescript: {
-//     outputFile: path.resolve(dirname, "payload-types.ts"),
-//   },
-
-//   db: mongooseAdapter({
-//     url: process.env.DATABASE_URL || "",
-//   }),
-
-//   sharp,
-
-//   plugins: [],
-
-//   // ✅ Startup log
-//   onInit: async () => {
-//     console.log("✅ Payload CMS started successfully");
-//     console.log("✅ MongoDB connection established");
-//   }
-// });

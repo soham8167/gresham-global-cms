d4da39c7e51c5e9e50fc349a2b65718b19a0dfe6
soham mondal <sohammondal50285@gmail.com>
2026-03-28 17:56:26 +0530

    add image and video size validation

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,5 +1,5 @@
+
 // import type { CollectionConfig } from 'payload'
-// // import { hasRole } from '@/utils/has-role'
 
 // export const Media: CollectionConfig = {
 //   slug: 'media',
@@ -16,8 +16,12 @@
 //       required: true,
 //     },
 //   ],
-//   upload: true,
+//   upload: {
+//     disableLocalStorage: true, //  don't save images on Render server disk
+//   },
 // }
+
+
 
 
 
@@ -31,12 +35,14 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+
   access: {
     read: () => true,
     create: () => true,
     update: () => true,
     delete: () => true,
   },
+
   fields: [
     {
       name: 'alt',
@@ -44,7 +50,32 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
+
   upload: {
-    disableLocalStorage: true, // ✅ don't save images on Render server disk
+    disableLocalStorage: true,
+    mimeTypes: ['image/*', 'video/*'], // allow only image & video
+  },
+
+  hooks: {
+    beforeChange: [
+      ({ req }) => {
+        const file = req.file;
+
+        if (!file) return;
+
+        const size = file.size;
+        const type = file.mimetype;
+
+        // Image limit (2MB)
+        if (type.startsWith('image') && size > 1 * 1024 * 1024) {
+          throw new Error('Image must be less than 2MB');
+        }
+
+        // Video limit (10MB)
+        if (type.startsWith('video') && size > 5 * 1024 * 1024) {
+          throw new Error('Video must be less than 10MB');
+        }
+      },
+    ],
   },
 }
